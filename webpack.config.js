82d89ab04c870be55bd57f3a4371cb1b8360d09a
86AhmedSoliman <asol1986ott@gmail.com>
2022-05-31 18:39:30 +0200

    finish components

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');//Opti
 module.exports= {
     entry: {
         'app': './src/index.js',
-        'assets/js/banner': './src/Assets/js/banner.js'
+        'assets/js/banner': './src/Assets/js/banner.js',
+        'assets/js/tabs': './src/Assets/js/tabs.js',
+        'assets/js/upload': './src/Assets/js/upload.js',
+        'assets/js/chart': './src/Assets/js/chart.js'
     },
     output: {
         publicPath: '/',
@@ -61,6 +64,19 @@ module.exports= {
                     }
                 ],
             },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                exclude: /fonts/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: "Assets/images",
+                        }
+                    }
+                ],
+            },
         ],
     },
     plugins: [
@@ -93,6 +109,51 @@ module.exports= {
             filename: 'Components/banner.html',
             template: './src/Components/banner.html',
             chunks: ['app','assets/js/banner']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/list.html',
+            template: './src/Components/list.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/tabs.html',
+            template: './src/Components/tabs.html',
+            chunks: ['app','assets/js/tabs']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/upload.html',
+            template: './src/Components/upload.html',
+            chunks: ['app','assets/js/upload']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/help.html',
+            template: './src/Components/help.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/summary.html',
+            template: './src/Components/summary.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/actions.html',
+            template: './src/Components/actions.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/sidebar.html',
+            template: './src/Components/sidebar.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/table.html',
+            template: './src/Components/table.html',
+            chunks: ['app']
+        }),
+        new HtmlWebPackPlugin({
+            filename: 'Components/chart.html',
+            template: './src/Components/chart.html',
+            chunks: ['app','assets/js/chart']
         }),
     ],
 }
